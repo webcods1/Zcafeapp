1747a8c14922f74ba8a75e4be296383a60160bf9
webcods1 <webcods1@gmail.com>
2025-12-14 20:43:27 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +8,16 @@ export default defineConfig({
   css: {
     postcss: './postcss.config.js',
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        purchase: resolve(__dirname, 'purchase.html'),
+        profile: resolve(__dirname, 'profile.html'),
+        bag: resolve(__dirname, 'bag.html'),
+        service: resolve(__dirname, 'service.html'),
+        wishlist: resolve(__dirname, 'wishlist.html')
+      }
+    }
+  }
 })
